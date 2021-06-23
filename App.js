8d96cf1a93ac8a57e99c19b1f1e3f48d59b85019
AG <415204643@qq.com>
2021-06-23 16:10:00 +0800

    引入Antd写一个demo

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div>
+      <Button type="primary">Parcel-React</Button>
+    </div>
+  )
+}
+
+export default App;
